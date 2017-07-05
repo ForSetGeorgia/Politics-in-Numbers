ea8fe442892ec8d7dd190eab7120af98504fbb0d
Antarya <antarya@gmail.com>
2017-07-05 09:48:27 +0400

    Staging repo update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :domain, 'pins.ge'
 set :user, 'pin'
 set :application, 'Politics-In-Numbers'
-set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
+set :repository, 'https://github.com/ForSetGeorgia/Politics-in-Numbers.git'
 set :branch, 'embed'
 set :web_url, "pins.ge"
 set :puma_thread_count_min, '1'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,9 @@ set :domain, 'alpha.jumpstart.ge'
 set :user, 'pin-staging'
 set :application, 'Politics-In-Numbers-Staging'
 # easier to use https; if you use ssh then you have to create key on server
-set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
+set :repository, 'https://github.com/ForSetGeorgia/Politics-in-Numbers.git'
 set :branch, 'highlight'
 set :web_url, 'dev-pin.jumpstart.ge'
 set :visible_to_robots, false
 set :use_ssl, true
+ForSetGeorgia
