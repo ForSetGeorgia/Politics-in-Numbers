0ebd416db03964155aff217c61f474f7f6968157
Antarya <antarya@gmail.com>
2017-07-17 15:47:24 +0400

    Hide highlights for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :domain, 'pins.ge'
 set :user, 'pin'
 set :application, 'Politics-In-Numbers'
 set :repository, 'https://github.com/ForSetGeorgia/Politics-in-Numbers.git'
-set :branch, 'embed'
+set :branch, 'highlight'
 set :web_url, "pins.ge"
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '8'
