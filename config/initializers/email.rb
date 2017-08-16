17bc1782fcf895227a58448ee3c0d6d7d1236ba8
Antarya <antarya@gmail.com>
2017-08-16 17:47:59 +0400

    Email config updated to use forset gmail accounts

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,9 +2,9 @@ if Rails.env.production? || Rails.env.staging?
   Rails.application.config.action_mailer.delivery_method = :smtp
 
   Rails.application.config.action_mailer.smtp_settings = {
-    address: 'box.pins.ge',
+    address: 'smtp.gmail.com',
     port: 587,
-    domain: 'pins.ge',
+    domain: 'forset.ge',
     user_name: ENV['APPLICATION_FEEDBACK_FROM_EMAIL'],
     password: ENV['APPLICATION_FEEDBACK_FROM_PWD'],
     authentication: :plain,
