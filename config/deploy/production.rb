1a41ace91bb2c3bab0a9ff161790c51a29df91b5
jason <jason.addie@gmail.com>
2017-10-31 11:13:08 +0400

    turning on ssl

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,6 @@ set :application, 'Politics-In-Numbers'
 set :repository, 'https://github.com/ForSetGeorgia/Politics-in-Numbers.git'
 set :branch, 'embed'
 set :web_url, "pins.ge"
-set :use_ssl, false
+set :use_ssl, true
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '8'
