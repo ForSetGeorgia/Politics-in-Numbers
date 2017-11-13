2c4d1946e44b1a1e7f6bd63d582bd59c0e45d1f7
Antarya <antarya@gmail.com>
2017-11-13 13:58:22 +0400

    Ru is on, with specific styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -76,4 +76,8 @@ module ApplicationHelper
   def lorem
     'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
   end
+
+  def upcase(str)
+    str.mb_chars.upcase.to_s
+  end
 end
