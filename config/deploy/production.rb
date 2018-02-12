56ee3384223ee62a0952fc8dd59c0391ac0fffac
Antarya <antarya@gmail.com>
2018-02-12 13:32:07 +0400

    Switch to https for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-set :domain, '178.62.74.79'
+set :domain, 'pins.ge'
 set :user, 'deploy'
 set :application, 'Politics-In-Numbers'
 set :repository, 'https://github.com/ForSetGeorgia/Politics-in-Numbers.git'
 set :branch, 'master'
 set :web_url, "pins.ge"
-set :use_ssl, false
+set :use_ssl, true
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '4'
